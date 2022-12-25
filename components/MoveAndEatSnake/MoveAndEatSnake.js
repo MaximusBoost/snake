42e16f7ec98cb32aa4266aa57b5234e368135231
Maxim <bekrinmks@gmail.com>
2022-12-25 13:57:59 +0300

    app Movich`s

--- components/MoveAndEatSnake/MoveAndEatSnake.js
+++ components/MoveAndEatSnake/MoveAndEatSnake.js
@@ -7,163 +7,213 @@ let index = createSnake()
 
 function moveSnake(numberSnake) {
     
-    let top = false
-    let right = false
-    let bottom = false
-    let left = false
-    let timerIdRight
-    let timerIdBottom
-    let timerIdLeft
-    let timerIdTop
-    let speed = 100
-    let allowed = true
+    let top = false;
+    let right = false;
+    let bottom = false;
+    let left = false;
+    let timerIdRight;
+    let timerIdBottom;
+    let timerIdLeft;
+    let timerIdTop;
+    let speed = 100;
     function checkFood() {
         if(TDS[numberSnake].lastElementChild) {
-            TDS[numberSnake].lastElementChild.remove()
-            addFood()
-            plusScore()
-        } 
-    }
+            TDS[numberSnake].lastElementChild.remove();
+            addFood();
+            plusScore();
+        };
+    };
 
-    // function fixRepeatMove(events) {
-    //     if (events.repeat != undefined) {
-    //         allowed = !events.repeat;
-    //       }
-    //       if (!allowed) return;
-    //       allowed = false;
-    // }
     
     function moveRight() {
             timerIdRight =  setInterval( () => {
-            TDS[numberSnake + 1].classList.add('snake')
-            TDS[numberSnake].classList.remove('snake')
-            numberSnake++
-            checkFood()
-        }, speed)
+            if(numberSnake%78 == 77) {
+                if(TDS[numberSnake]) TDS[numberSnake].classList.remove('snake');
+                numberSnake -= 78;
+            };
+
+
+
+            if(TDS[numberSnake + 1]) TDS[numberSnake + 1].classList.add('snake');
+
+            if(counterScore) {
+                if(TDS[numberSnake - counterScore]) TDS[numberSnake - counterScore].classList.remove('snake');
+                console.log(44444);
+            } else {
+                if(TDS[numberSnake]) TDS[numberSnake].classList.remove('snake');
+            }
+            // if(counterScore) increaseSnake(counterScore, 'right', numberSnake);
+            numberSnake++;
+            checkFood();
+            console.log('---------');
+        }, speed);
     }
 
     function moveLeft() {
             timerIdLeft = setInterval( () => {
-            TDS[numberSnake - 1].classList.add('snake')
-            TDS[numberSnake].classList.remove('snake')
-            numberSnake--
-            checkFood()
-        },speed)
+                if(numberSnake%78 == 0) {
+                if(TDS[numberSnake]) TDS[numberSnake].classList.remove('snake');
+                numberSnake += 78;
+            };
+
+            if(TDS[numberSnake]) TDS[numberSnake - 1].classList.add('snake');
+            // if(TDS[numberSnake]) TDS[numberSnake].classList.remove('snake');
+
+            if(counterScore) {
+                if(TDS[numberSnake + counterScore]) TDS[numberSnake + counterScore].classList.remove('snake');
+                console.log(44444);
+            } else {
+                if(TDS[numberSnake]) TDS[numberSnake].classList.remove('snake');
+            }
+
+            numberSnake--;
+            checkFood();
+        },speed);
     }
 
     function moveTop() {
             timerIdTop =  setInterval( () => {
-            TDS[numberSnake - 78].classList.add('snake')
-            TDS[numberSnake].classList.remove('snake')
-            numberSnake = numberSnake - 78
-            checkFood()
-        },speed)
+            if(numberSnake < 78) {
+                TDS[numberSnake].classList.remove('snake');
+                let num = 2652;
+                numberSnake += num;
+            }
+
+            TDS[numberSnake - 78].classList.add('snake');
+            // if (TDS[numberSnake]) TDS[numberSnake].classList.remove('snake');
+
+            if(counterScore) {
+                if(TDS[numberSnake + 78*+counterScore]) TDS[numberSnake + 78*+counterScore].classList.remove('snake');
+                console.log(44444);
+            } else {
+                if(TDS[numberSnake]) TDS[numberSnake].classList.remove('snake');
+            }
+
+            numberSnake = numberSnake - 78;
+            checkFood();
+        },speed);
     }
 
     function moveBottom() {
+
             timerIdBottom = setInterval( () => {
-            TDS[numberSnake + 78].classList.add('snake')
-            TDS[numberSnake].classList.remove('snake')
-            numberSnake = numberSnake + 78
-            checkFood()
-        },speed)
+            if(numberSnake >= 2574) {
+                TDS[numberSnake].classList.remove('snake');
+                let num = 2652;
+                numberSnake -= num;
+            }
+
+            TDS[numberSnake + 78].classList.add('snake');
+            // if(TDS[numberSnake]) TDS[numberSnake].classList.remove('snake');
+
+            if(counterScore) {
+                if(TDS[numberSnake - 78*+counterScore]) TDS[numberSnake - 78*+counterScore].classList.remove('snake');
+            } else {
+                if(TDS[numberSnake]) TDS[numberSnake].classList.remove('snake');
+            }
+
+            numberSnake = numberSnake + 78;
+            checkFood();
+        },speed);
     }
 
     document.addEventListener('keydown', function(event) {
-        
         if( event.code.toLowerCase() == 'arrowdown') {
-
+            if (event.repeat == true) return;
 
             if(left) {
-                clearInterval(timerIdLeft)
-                left = false
+                clearInterval(timerIdLeft);
+                left = false;
             }
             if(top) {
-                clearInterval(timerIdTop)
-                top = false
+                clearInterval(timerIdTop);
+                top = false;
             }
             if(right) {
-                clearInterval(timerIdRight)
-                right = false
+                clearInterval(timerIdRight);
+                right = false;
             }
             if(bottom) {
-                clearInterval(timerIdBottom)
+                clearInterval(timerIdBottom);
             }   
-            bottom = true
-            moveBottom()
+            bottom = true;
+            moveBottom();
         }
     })
 
     document.addEventListener('keydown', function (event) {
         if( event.code.toLowerCase() == 'arrowleft') {
+            if (event.repeat == true) return;
 
             if(bottom) {
-                clearInterval(timerIdBottom)
-                bottom = false
+                clearInterval(timerIdBottom);
+                bottom = false;
             }
             if(top) {
-                clearInterval(timerIdTop)
-                top = false
+                clearInterval(timerIdTop);
+                top = false;
             }
             if(right) {
-                clearInterval(timerIdRight)
-                right = false
+                clearInterval(timerIdRight);
+                right = false;
             }
             if(left) {
-                clearInterval(timerIdLeft)
+                clearInterval(timerIdLeft);
             }
-            left = true
-            moveLeft()
+            left = true;
+            moveLeft();
             
         }
     })
 
     document.addEventListener('keydown', function (event) {
         if( event.code.toLowerCase() == 'arrowup') {
+            if (event.repeat == true) return;
 
             if(left) {
-                clearInterval(timerIdLeft)
-                left = false
+                clearInterval(timerIdLeft);
+                left = false;
             }
             if(bottom) {
-                clearInterval(timerIdBottom)
-                bottom = false
+                clearInterval(timerIdBottom);
+                bottom = false;
             }
             if(right) {
-                clearInterval(timerIdRight)
-                right = false
+                clearInterval(timerIdRight);
+                right = false;
             }
             if(top) {
-                clearInterval(timerIdTop)
+                clearInterval(timerIdTop);
             }
-            top = true
-            moveTop()
+            top = true;
+            moveTop();
             
         }
     })
 
     document.addEventListener('keydown', function (event) {
         if( event.code.toLowerCase() == 'arrowright') {
-            
+            if (event.repeat == true) return;
+
             if(left) {
-                clearInterval(timerIdLeft)
-                left = false
+                clearInterval(timerIdLeft);
+                left = false;
             }
             if(top) {
-                clearInterval(timerIdTop)
-                top = false
+                clearInterval(timerIdTop);
+                top = false;
             }
             if(bottom) {
-                clearInterval(timerIdBottom)
-                bottom = false
+                clearInterval(timerIdBottom);
+                bottom = false;
             }
             if(right) {
-                clearInterval(timerIdRight)
+                clearInterval(timerIdRight);
             }
-            right = true
-            moveRight()
+            right = true;
+            moveRight();
             
         }
     })
 }
-moveSnake(index)
+moveSnake(index);
